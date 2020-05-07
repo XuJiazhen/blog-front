7ba7924b2e64463d83ab8029c16b4286c02919a5
XuJiazhen <xu.159623@gmail.com>
2020-05-07 15:12:14 +0800

    deploy config

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 export default {
   BASE_URL:
     process.env.NODE_ENV === 'production'
-      ? 'http://120.27.63.13:8130/'
-      : 'http://localhost:8130/'
+      ? 'http://api.xujiazhen.website/api/'
+      : 'http://localhost:8130/api/'
 }
